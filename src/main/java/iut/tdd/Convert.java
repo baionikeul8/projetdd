fdfa2501a62ad36ee02c5487b103cd781172000a
Quentin Spinnewyn <quentin.spinnewyn@etudiant.univ-lille1.fr>
2015-04-03 17:31:32 +0200

    envoi_message

--- src/main/java/iut/tdd/Convert.java
+++ src/main/java/iut/tdd/Convert.java
@@ -8,6 +8,9 @@ public class Convert {
 	static String[]  chiffres = {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","20","30","40","50","60","100","1000","1000000","1000000000","1000000000000"};
 	static String[]  chiffreslettres = {"zéro","un","deux","trois","quatre","cinq","six","sept","huit","neuf","dix","onze","douze","treize","quatorze","quinze","seize","vingt","trente","quarante","cinquante","soixante","cent","mille","million","milliard","billion"};
 	
+	static String[] cch={"10","7","8","9"};
+	static String[] cchh={"dix","sept","huit","neuf"};
+	
 	public static String num2text(String input) {
 		for (int i=0 ; i<chiffres.length;i++){
 			if(chiffres[i].equals(input)){
